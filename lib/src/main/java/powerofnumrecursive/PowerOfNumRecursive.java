edf46d2b0731dd13abe09f74dbadadec5812c12a
dakinestyle <ryanoatsie@outlook.com>
2024-10-22 14:31:04 -0700

    Refactor PowerOfNumRecursive class to remove unused variables and optimize power calculation

--- lib/src/main/java/powerofnumrecursive/PowerOfNumRecursive.java
+++ lib/src/main/java/powerofnumrecursive/PowerOfNumRecursive.java
@@ -1,8 +1,5 @@
 // power of a number using recursion
 public class PowerOfNumRecursive {
- 
-    private int num;
-    private int exp;
     public int power(int num, int exp)
     {  
         if(exp == 0)
