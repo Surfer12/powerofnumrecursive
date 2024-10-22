f1317f77e24aceb268eab1125eb80578d8812847
dakinestyle <ryanoatsie@outlook.com>
2024-10-22 14:27:45 -0700

    Refactor PowerOfNumRecursive class to optimize power calculation

--- lib/src/main/java/powerofnumrecursive/PowerOfNumRecursive.java
+++ lib/src/main/java/powerofnumrecursive/PowerOfNumRecursive.java
@@ -12,3 +12,9 @@ public class PowerOfNumRecursive {
         return num *= power(num, exp - 1);
     }
 }
+
+// the above function is not optimized and takes o(n) time complexity
+// the space complexity is o(n) because the recursive call stack is used to store the intermediate results
+// the time complexity is o(n) because we are calling the function recursively and decrementing the exponent by 1 in each call
+// The o(n) time complexity is due to the fact that the function is called recursively and the exponent is decremented by 1 in each call so the function is called n times
+// the space complexity is o(n) because the recursive call stack is used to store the intermediate results
