9dc6bea8970a578635795784e8f0fc1086077970
dakinestyle <ryanoatsie@outlook.com>
2024-10-23 23:29:38 -0700

    Refactor power calculation in mainMethod.java

--- lib/src/main/java/powerofnumrecursive/mainMethod.java
+++ lib/src/main/java/powerofnumrecursive/mainMethod.java
@@ -5,8 +5,8 @@ public static void main(String[] args) {
     PowerOfNumRecursiveImproved obj = new PowerOfNumRecursiveImproved();
     System.out.println(obj.power(2, 3));
     RecursiveReduced obj2 = new RecursiveReduced();
-    System.out.println(obj2.power(2, 3));
+    System.out.println(obj2.power(3, 3));
     UnusureOfOptimizedPower obj3 = new UnusureOfOptimizedPower();
-    System.out.println(obj3.power(2, 3));
+    System.out.println(obj3.power(4, 3));
 }
 }
